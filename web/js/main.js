38a1110bd7af281cc9f027399f0bf27a9b9e67b7
Unbroken Technology <s.albert.staton@gmail.com>
2019-04-02 13:07:15 -0400

    working clock

--- web/js/main.js
+++ web/js/main.js
@@ -1,18 +1,25 @@
-var clock = new Date();
-var hours = clock.getHours ();
-var minutes = clock.getMinutes ();
-var seconds = clock.getSeconds ();
-var timeOfDay = ( hours < 12 ) ? "AM" : "PM";
+var countdown;
 
-console.log(minutes);
-console.log(hours);
-console.log(seconds);
 function time()  {
+    var clock = new Date();
+    var hours = clock.getHours ();
+    var minutes = clock.getMinutes ();
+    var seconds = clock.getSeconds ();
+    var timeOfDay = ( hours < 12 ) ? "AM" : "PM";
+
     
     hours = ( hours > 12 ) ? hours - 12 : hours;
     minuets = ( minutes < 10 ? "0" : "" ) + minutes;
     seconds = ( seconds < 10 ? "0" : "" ) + seconds;
     
-
-  
+    var currentTime= hours + ":" + minutes + ":" + seconds + " " + timeOfDay;
+    document.getElementById("clock").innerHTML = currentTime;
+    
+    
+    
+    // console.log(minutes);
+    // console.log(hours);
+    // console.log(seconds);
+    
+     countdown = setTimeout ( time, 1000);
 }
