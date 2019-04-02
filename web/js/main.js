9ae95dfe9920b8eabae40db52c00c431fa0f2aa0
Unbroken Technology <s.albert.staton@gmail.com>
2019-04-02 11:13:21 -0400

    write function

--- web/js/main.js
+++ web/js/main.js
@@ -1,28 +1,18 @@
-function updateClock ( )
-{
-  var currentTime = new Date ( );
+var clock = new Date();
+var hours = clock.getHours ();
+var minutes = clock.getMinutes ();
+var seconds = clock.getSeconds ();
+var timeOfDay = ( hours < 12 ) ? "AM" : "PM";
 
-  var currentHours = currentTime.getHours ( );
-  var currentMinutes = currentTime.getMinutes ( );
-  var currentSeconds = currentTime.getSeconds ( );
+console.log(minutes);
+console.log(hours);
+console.log(seconds);
+function time()  {
+    
+    hours = ( hours > 12 ) ? hours - 12 : hours;
+    minuets = ( minutes < 10 ? "0" : "" ) + minutes;
+    seconds = ( seconds < 10 ? "0" : "" ) + seconds;
+    
 
-  // Pad the minutes and seconds with leading zeros, if required
-  currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
-  currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
-
-  // Choose either "AM" or "PM" as appropriate
-  var timeOfDay = ( currentHours < 12 ) ? "AM" : "PM";
-
-  // Convert the hours component to 12-hour format if needed
-  currentHours = ( currentHours > 12 ) ? currentHours - 12 : currentHours;
-
-  // Convert an hours component of "0" to "12"
-  currentHours = ( currentHours == 0 ) ? 12 : currentHours;
-
-  // Compose the string for display
-  var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
-
-  // Update the time display
-  document.getElementById("clock").firstChild.nodeValue = currentTimeString;
+  
 }
-
